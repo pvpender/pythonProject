5c4f088e1b8e086dd8c5be8f77c9df64d3574a6a
Николай Моисеев <ndmoiseev@miem.hse.ru>
2022-03-12 17:54:08 +0300

    new files

---
+++ Work/Library/standard_functions.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+
+class Database:
+    """
+    Класс в котором реализуется работа с БД
+    """
+    __connection = sqlite3.Connection
+    __name = ""
+    __cursor = sqlite3.Cursor
+    __columns = []
+
+    def __init__(self, path: str, table_name: str, columns: list):
+        """
+        Инициализация базы данных
+        :param path(Str): Путь к файлу с базой данных
+        :param table_name(Str): Имя таблицы в базе
+        :param columns(List of Str): Список столбцов
+        :return None
+        author Moiseev Nicolay
+        """
+        self.__connection = sqlite3.connect(path)
+        self.__cursor = self.__connection.cursor()
+        self.__name = table_name
+        self.__columns = columns
+        self.create_table()
+
+    def create_table(self):
+        request = f"CREATE TABLE IF NOT EXISTS {self.__name}("
+        for i in self.__columns:
+            request += f"{i},"
+        request = request[0:-1] + ")"
+        self.__cursor.execute(request)
+
+    def insert_row(self):
+        pass
+
+    def del_row(self):
+        pass
+
+    def find_data(self):
+        pass
---
+++ Work/Scripts/main.py
@@ -0,0 +1,7 @@
+from Work.Library.standard_functions import *
+from application_functions import *
+import sqlite3
+
+if __name__ == "__main__":
+    first_base = Database("../Data/test.db", "test", ["a", "b", "c"])
+

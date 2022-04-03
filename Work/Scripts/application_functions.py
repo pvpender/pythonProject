26c41ea5f14d11dd0b05a0211c698148251d506c
Николай Моисеев <nicolaymoiseev1@gmail.com>
2022-04-03 16:41:15 +0300

    improve parsing algorithm

--- Work/Scripts/application_functions.py
+++ Work/Scripts/application_functions.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import re
 from bs4 import BeautifulSoup
 import requests as req
 
@@ -15,14 +16,19 @@ class Parser:
 
     def __init__(self):
         self.__page = req.get(self.__url, headers=self.__headers)
+        soup = BeautifulSoup(self.__page.text, "lxml")
+        self.__links = [tag.find("a")["href"] for tag in soup.select("td:has(a)")
+                        if tag.find("a")["href"].count("covid-19")]
 
     __url = "https://gogov.ru/covid-19/world"
     __page = req.Response
+    __links = []
     __headers = {
         'Connection': 'keep-alive',
         'Cache-Control': 'max-age=0',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'DNT': '1',
         'Accept-Encoding': 'gzip, deflate, lzma, sdch',
@@ -30,17 +36,17 @@ class Parser:
     }
 
     @staticmethod
-    def __none_testing(string: str):
+    def __none_testing(string: re.Match) -> str:
         """
         Функция проверки строку на пустоту
-        :param string: Строка
+        :param string: объект класса re.Match
         :return: Саму строку или "0"
         author Moiseev Nicolay
         """
-        if string == "":
+        if string is None:
             return "0"
         else:
-            return string
+            return string.group(0)
 
     def get_info(self) -> dict:
         """Функция получения чего-то
@@ -51,7 +57,8 @@ class Parser:
         mas = list(map(str, soup.tbody.text.split("\n")))[2:]
         output_dict = {}
         for i in range(0, len(mas), 16):
-            output_dict.update({mas[i]: [self.__none_testing(mas[i + 2][1:]), self.__none_testing(mas[i + 7][1:])]})
+            output_dict.update({mas[i]: [self.__none_testing(re.search(r"\d+", mas[i + 2])),
+                                         self.__none_testing(re.search(r"\d+", mas[i + 7]))]})
         return output_dict
 
     def get_info_new_disease(self) -> dict:
@@ -64,7 +71,7 @@ class Parser:
         mas = list(map(str, soup.tbody.text.split("\n")))[2:]
         output_dict = {}
         for i in range(0, len(mas), 16):
-            output_dict.update({mas[i]: self.__none_testing(mas[i + 2][1:])})
+            output_dict.update({mas[i]: self.__none_testing(re.search(r"\d+", mas[i + 2]))})
         return output_dict
 
     def get_info_new_dies(self) -> dict:
@@ -77,5 +84,5 @@ class Parser:
         mas = list(map(str, soup.tbody.text.split("\n")))[2:]
         output_dict = {}
         for i in range(0, len(mas), 16):
-            output_dict.update({mas[i]: self.__none_testing(mas[i + 7][1:])})
+            output_dict.update({mas[i]: self.__none_testing(re.search(r"\d+", mas[i + 7][1:]))})
         return output_dict
